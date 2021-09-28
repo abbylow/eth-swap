1abc0d3c8a7a5079d2ca3360204f2f05d4eff875
Abby Low <abbylow@Abbys-MacBook-Pro.local>
2021-09-29 00:00:41 +0800

    feat: frontend buy token

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 import Navbar from './NavBar';
+import Main from './Main';
+import Token from '../abis/Token.json'
+import EthSwap from '../abis/EthSwap.json'
 import './App.css';
 
 class App extends Component {
@@ -11,12 +14,35 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3 
+    const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
 
     const ethBalance = await web3.eth.getBalance(this.state.account)
     this.setState({ ethBalance })
+
+    // load token contract
+    const networkId = await web3.eth.net.getId()
+    const tokenData = Token.networks[networkId]
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address)
+      this.setState({ token })
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      this.setState({ tokenBalance: tokenBalance.toString() })
+    } else {
+      window.alert('Token contract not deployed to detected network. ')
+    }
+
+    // load eth swap contract
+    const ethSwapData = EthSwap.networks[networkId]
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap })
+    } else {
+      window.alert('EthSwap contract not deployed to detected network. ')
+    }
+
+    this.setState({ loading: false })
   }
 
   async loadWeb3() {
@@ -32,23 +58,47 @@ class App extends Component {
     }
   }
 
+  buyTokens = (etherAmount) => {
+    this.setState({ loading: true })
+    this.state.ethSwap.methods.buyTokens()
+      .send({ from: this.state.account, value: etherAmount })
+      .on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       account: '',
-      ethBalance: '0'
+      token: {},
+      tokenBalance: '0',
+      ethSwap: {},
+      ethBalance: '0',
+      loading: true,
     }
   }
 
   render() {
+    let content
+    if (this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main
+        tokenBalance={this.state.tokenBalance}
+        ethBalance={this.state.ethBalance}
+        buyTokens={this.buyTokens}
+      />
+    }
+
     return (
       <div>
         <div className="container-fluid mt-5">
           <Navbar account={this.state.account} />
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }} >
               <div className="content mr-auto ml-auto">
-                <h1>Hello World</h1>
+                {content}
               </div>
             </main>
           </div>
